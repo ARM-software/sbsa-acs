62c3b123b97a9fed1183ede6ff67e0166f050149
Sakar Arora <sakar.arora@arm.com>
2018-03-13 17:20:24 +0530

    uefi_app: increase iovirt table size
    Some crashes were reported w.r.t IORT
    table parsing. Based on the information
    provided, insufficient memory for
    iovirt table seems a possible root cause.
    Hence, increasing its size.
    
    Signed-off-by: Sakar Arora <sakar.arora@arm.com>

--- uefi_app/SbsaAvs.h
+++ uefi_app/SbsaAvs.h
@@ -30,7 +30,7 @@
   #define TIMER_INFO_TBL_SZ      1024 /*Supports maximum 2 system timers*/
   #define WD_INFO_TBL_SZ         512  /*Supports maximum 20 Watchdogs*/
   #define MEM_INFO_TBL_SZ        4096 /*Supports maximum 110 memory regions*/
-  #define IOVIRT_INFO_TBL_SZ     4096 /*Supports maximum 60 nodes of a typical iort table*/
+  #define IOVIRT_INFO_TBL_SZ     8192 /*Supports maximum 120 nodes of a typical iort table*/
   #define PERIPHERAL_INFO_TBL_SZ 1024 /*Supports maximum 20 PCIe EPs (USB and SATA controllers only) */
   #define PCIE_INFO_TBL_SZ       512  /*Supports maximum 20 RC's*/
 
