c1135038c20b35fab11a2b4ec49000cce4cf5814
Ajayswar S <ajayswar.s@arm.com>
2024-10-10 01:08:29 +0530

    Errata 669: FEAT_LPA and FEAT_LPA2 Check
    -Adding additional FEAT_LPA2 check if FEAT_LPA is implemented
    -If FEAT_LPA2 is implemented then test will pass
    
    Signed-off-by Ajayswar S <ajayswar.s@arm.com>
    
    Change-Id: I78c6e3a17b3ed4d89b1e91327aacc90a331aba75

--- test_pool/pe/operating_system/test_c004.c
+++ test_pool/pe/operating_system/test_c004.c
@@ -31,17 +31,31 @@
 
 static void payload(void)
 {
-    uint64_t data = 0;
+    uint64_t data1 = 0, data2 = 0;
     uint32_t index = val_pe_get_index_mpid(val_pe_get_mpid());
     uint64_t peri_count;
     uint64_t peri_base;
 
     /* ID_AA64MMFR0_EL1.PARange [3:0] = 0b0110 indicates FEAT_LPA is implemented */
-    data = VAL_EXTRACT_BITS(val_pe_reg_read(ID_AA64MMFR0_EL1), 0, 3);
+    data1 = VAL_EXTRACT_BITS(val_pe_reg_read(ID_AA64MMFR0_EL1), 0, 3);
     peri_count = val_peripheral_get_info(NUM_ALL, 0);
+    data2 = val_pe_reg_read(ID_AA64MMFR0_EL1);
 
-    if (data == FEAT_LPA_IMPL)
+
+    if (data1 == FEAT_LPA_IMPL)
     {
+        /* The following fields indicates FEAT_LPA2 is implemented:
+               ID_AA64MMFR0_EL1.TGran16 [23:20] == 0b0010 and
+               ID_AA64MMFR0_EL1.TGran4  [31:28] == 0b0001
+         */
+
+        if (VAL_EXTRACT_BITS(data2, 20, 23) == 0x2 || VAL_EXTRACT_BITS(data2, 28, 31) == 0x1)
+        {
+            val_print(ACS_PRINT_INFO, "\n System supports both FEAT_LPA & FEAT_LPA2 ", 0);
+            val_set_status(index, RESULT_PASS(TEST_NUM, 01));
+            return;
+        }
+
         /* If the PE implements FEAT_LPA Index through Peripheral info table and
                                                               get base addresses */
         while (peri_count)
@@ -57,7 +71,7 @@ static void payload(void)
             peri_count--;
         }
 
-        val_set_status(index, RESULT_PASS(TEST_NUM, 01));
+        val_set_status(index, RESULT_PASS(TEST_NUM, 02));
     }
     else
     {
