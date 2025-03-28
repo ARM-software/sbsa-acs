bcbaaf1c75e1687c31cb5a2f8fb53c651ec6d2a4
Sujana-M <52772661+Sujana-M@users.noreply.github.com>
2025-03-28 09:52:19 +0530

    Usage of VAL calls instead of UEFI calls (#544)
    - Made changes in SbsaAvsMain.c and SbsaAvs.h to have
      VAL calls instead of EFI calls from uefi_app to ensure
      the VAL/PAL flow.
    - Changes in copyright
    - Removal of trailing spaces
    
    Signed-off-by: kiranrnad <kirankumarayya.ronad@arm.com>

--- uefi_app/SbsaAvs.h
+++ uefi_app/SbsaAvs.h
@@ -18,61 +18,61 @@
 #ifndef __SBSA_AVS_LEVEL_H__
 #define __SBSA_AVS_LEVEL_H__
 
+#define SIZE_4K                  0x1000
 
+#define SBSA_ACS_MAJOR_VER       7
+#define SBSA_ACS_MINOR_VER       2
+#define SBSA_ACS_SUBMINOR_VER    2
 
-  #define SBSA_ACS_MAJOR_VER      7
-  #define SBSA_ACS_MINOR_VER      2
-  #define SBSA_ACS_SUBMINOR_VER   2
+#define G_SBSA_LEVEL             4
+#define SBSA_MIN_LEVEL_SUPPORTED 3
+#define SBSA_MAX_LEVEL_SUPPORTED 7
+#define G_PRINT_LEVEL            ACS_PRINT_TEST
 
-  #define G_SBSA_LEVEL  4
-  #define SBSA_MIN_LEVEL_SUPPORTED 3
-  #define SBSA_MAX_LEVEL_SUPPORTED 7
-  #define G_PRINT_LEVEL ACS_PRINT_TEST
+/* Note 1: Total Size Required for Info tables ~ 1200 KB
+  * Table size is required to be updated whenever new members
+  * are added in the info table structures
+  */
 
-  /* Note 1: Total Size Required for Info tables ~ 1200 KB
-   * Table size is required to be updated whenever new members
-   * are added in the info table structures
-   */
-
-  /* Please MAKE SURE all the table sizes are 16 Bytes aligned */
-  #define PE_INFO_TBL_SZ         16384  /*Supports maximum 400 PEs*/
+/* Please MAKE SURE all the table sizes are 16 Bytes aligned */
+#define PE_INFO_TBL_SZ           16384  /*Supports maximum 400 PEs*/
                                         /*[40 B Each + 4 B Header] */
-  #define GIC_INFO_TBL_SZ        240000 /* Supports maximum 832 gic info */
+#define GIC_INFO_TBL_SZ          240000 /* Supports maximum 832 gic info */
                                         /* (GICH, CPUIF, RD, ITS, MSI, D) */
                                         /*[48 B Each + 24 B Header]*/
-  #define TIMER_INFO_TBL_SZ      2048   /*Supports maximum 4 system timers*/
+#define TIMER_INFO_TBL_SZ        2048   /*Supports maximum 4 system timers*/
                                         /*[248 B Each + 52 B Header]  */
-  #define WD_INFO_TBL_SZ         512    /*Supports maximum 20 Watchdogs*/
+#define WD_INFO_TBL_SZ           512    /*Supports maximum 20 Watchdogs*/
                                         /*[24 B Each + 4 B Header] */
-  #define MEM_INFO_TBL_SZ        32768  /*Supports maximum 800 memory regions*/
+#define MEM_INFO_TBL_SZ          32768  /*Supports maximum 800 memory regions*/
                                         /*[40 B Each + 16 B Header]      */
-  #define IOVIRT_INFO_TBL_SZ     1048576/*Supports maximum 2400 nodes of a typical iort table*/
+#define IOVIRT_INFO_TBL_SZ       1048576/*Supports maximum 2400 nodes of a typical iort table*/
                                         /*[(268+32*5) B Each + 24 B Header]*/
-  #define PERIPHERAL_INFO_TBL_SZ 2048   /*Supports maximum 20 PCIe EPs */
+#define PERIPHERAL_INFO_TBL_SZ   2048   /*Supports maximum 20 PCIe EPs */
                                         /*(USB and SATA controllers only) */
                                         /*[56 B Each + 16 B Header]*/
-  #define PCIE_INFO_TBL_SZ       1024   /*Supports maximum 40 RC's*/
+#define PCIE_INFO_TBL_SZ         1024   /*Supports maximum 40 RC's*/
                                         /*[24 B Each + 4 B Header]*/
-  #define PMU_INFO_TBL_SZ        20496  /*Supports maximum 512 PMUs*/
+#define PMU_INFO_TBL_SZ          20496  /*Supports maximum 512 PMUs*/
                                         /*[40 B Each + 4 B Header]*/
-  #define RAS_INFO_TBL_SZ        40960  /*Supports maximum 256 RAS Nodes*/
+#define RAS_INFO_TBL_SZ          40960  /*Supports maximum 256 RAS Nodes*/
                                         /*[144 B Each + 12 B Header]*/
-  #define RAS2_FEAT_INFO_TBL_SZ  20480  /*Supports maximum of 1024 RAS2 memory feature entries*/
+#define RAS2_FEAT_INFO_TBL_SZ    20480  /*Supports maximum of 1024 RAS2 memory feature entries*/
                                         /*[16 B Each + 8 B Header]*/
-  #define CACHE_INFO_TBL_SZ      262144 /*Support maximum of 7280 cache entries*/
+#define CACHE_INFO_TBL_SZ        262144 /*Support maximum of 7280 cache entries*/
                                         /*[36 B Each + 4 B Header]*/
-  #define SRAT_INFO_TBL_SZ       16384  /*Support maximum of 500 mem proximity domain entries*/
+#define SRAT_INFO_TBL_SZ         16384  /*Support maximum of 500 mem proximity domain entries*/
                                         /*[32 B Each + 8 B Header]*/
-  #define MPAM_INFO_TBL_SZ       262144 /*Supports maximum of 1800 MSC entries*/
+#define MPAM_INFO_TBL_SZ         262144 /*Supports maximum of 1800 MSC entries*/
                                         /*[24+(24*5) B Each + 4 B Header]*/
-  #define HMAT_INFO_TBL_SZ       12288  /*Supports maximum of 400 Proximity domains*/
+#define HMAT_INFO_TBL_SZ         12288  /*Supports maximum of 400 Proximity domains*/
                                         /*[24 B Each + 8 B Header]*/
-  #define PCC_INFO_TBL_SZ        262144 /*Supports maximum of 234 PCC info entries*/
-                                        /*[112 B Each + 4B Header]*/
+#define PCC_INFO_TBL_SZ          262144 /*Supports maximum of 234 PCC info entries*/
+                                      /*[112 B Each + 4B Header]*/
 
-  #ifdef _AARCH64_BUILD_
-  unsigned long __stack_chk_guard = 0xBAAAAAAD;
-  unsigned long __stack_chk_fail =  0xBAAFAAAD;
-  #endif
+#ifdef _AARCH64_BUILD_
+unsigned long __stack_chk_guard = 0xBAAAAAAD;
+unsigned long __stack_chk_fail =  0xBAAFAAAD;
+#endif
 
 #endif
